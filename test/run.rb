4bd526454b0d9d51209da2ebba48ec285c4ddc0c
Andrew Konchin <andry.konchin@gmail.com>
2024-09-17 13:56:43 +0300

    Skip building on TruffleRuby

--- test/run.rb
+++ test/run.rb
@@ -3,8 +3,8 @@
 $VERBOSE = true
 
 source_dir = "#{__dir__}/.."
-$LOAD_PATH.unshift("#{source_dir}/test")
-$LOAD_PATH.unshift("#{source_dir}/lib")
+$LOAD_PATH.unshift(File.expand_path("#{source_dir}/test"))
+$LOAD_PATH.unshift(File.expand_path("#{source_dir}/lib"))
 
 build_dir = Dir.pwd
 if File.exist?("#{build_dir}/fiddle.so")
